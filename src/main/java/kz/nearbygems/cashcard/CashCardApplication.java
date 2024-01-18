ca2c0cb968f84b8573fc268eb860fd150b7a8f18
nearbygems <bergen.asymov@gmail.com>
2024-01-18 22:23:44 +0600

    implementing GET

--- src/main/java/kz/nearbygems/cashcard/CashCardApplication.java
+++ src/main/java/kz/nearbygems/cashcard/CashCardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CashCardApplication {
 
-  public static void Ïmain(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(CashCardApplication.class, args);
   }
 
